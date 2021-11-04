144391d330900b0d7cdfcf0c510fa4d7b584bb4c
Brian Dilts <brianedilts@gmail.com>
2021-11-04 09:17:58 -0600

    Initial Commit

---
+++ yelp2/yelp2App.swift
@@ -0,0 +1,17 @@
+//
+//  yelp2App.swift
+//  yelp2
+//
+//  Created by Brian Dilts on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct yelp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
